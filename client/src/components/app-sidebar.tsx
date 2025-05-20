39ad0a8ad9069bdcbba24693f703c5a30accea20
b-krol <bartosz.krol777@gmail.com>
2025-05-20 18:07:38 +0200

    sidebar now shows items from list

--- client/src/components/app-sidebar.tsx
+++ client/src/components/app-sidebar.tsx
@@ -1,3 +1,18 @@
+const listItems = [
+  {
+    text: "el1",
+    url: "#",
+  },
+  {
+    text: "el2",
+    url: "#",
+  },
+  {
+    text: "el3",
+    url: "#",
+  }
+];
+
 export function AppSidebar() {
   return (
     <ul className="sidebar">
@@ -5,12 +20,12 @@ export function AppSidebar() {
         <h1 className="sidebar-head-title">Menu</h1>
       </ul>
       <ul className="sidebar-list">
+        {listItems.map((item) => (
           <li className="sidebar-list-item">
             {/*<item.icon className="sidebar-icon mr-2 h-6 w-6 inline-block stroke-blue-500 group-hover:stroke-blue-200 duration-300" />*/}
-            <span className="">
-              Element menu
-            </span>
+            <span className="">{item.text}</span>
           </li>
+        ))}
       </ul>
     </ul>
   );
